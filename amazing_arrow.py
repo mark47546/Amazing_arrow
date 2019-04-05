dbaadd5a15ea27d9e3b8b546dde70c61ef9e67e6
mark47546 <mark47546natthaphon@gmail.com>
2019-04-05 14:32:24 +0700

    create background

--- amazing_arrow.py
+++ amazing_arrow.py
@@ -4,17 +4,20 @@ from models import World
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-class Background(arcade.Window):
+class Window(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
         self.world = World(width, height)
-        # self.background = arcade.load_texture('background.jpg')
- 
-        arcade.set_background_color(arcade.color.GREEN)
+        self.background = None
+        # self.ship_sprite = ModelSprite('robin.jpg',model=self.world.robin)
 
+    def setup(self):
+        self.background = arcade.load_texture('Y1T2\\amazing_arrow\\background.png')
     def on_draw(self):
         arcade.start_render()
+        arcade.draw_texture_rectangle(SCREEN_WIDTH//2, SCREEN_HEIGHT//2,
+                                    SCREEN_WIDTH,SCREEN_HEIGHT, self.background)
         arcade.draw_text(str(self.world.score),
                          self.width - 30, self.height - 30,
                          arcade.color.WHITE, 20)
@@ -23,9 +26,33 @@ class Background(arcade.Window):
         self.world.update(delta) 
 
 def main():
-    Background(SCREEN_WIDTH,SCREEN_HEIGHT)
-    arcade.run
+    Window(SCREEN_WIDTH,SCREEN_HEIGHT)
+    arcade.run()
     
 if __name__ == '__main__':
-    window = Background(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window = Window(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window.setup()
     arcade.run()
+
+# class ModelSprite(arcade.Sprite):
+#     def __init__(self, *args, **kwargs):
+#         self.model = kwargs.pop('model', None)
+ 
+#         super().__init__(*args, **kwargs)
+ 
+#     def sync_with_model(self):
+#         if self.model:
+#             self.set_position(self.model.x, self.model.y)
+ 
+#     def draw(self):
+#         self.sync_with_model()
+#         super().draw()
+    
+#     def sync_with_model(self):
+#         if self.model:
+#             self.set_position(self.model.x, self.model.y)
+#             self.angle = self.model.angle
+
+#     def update(self, delta):
+#         self.world.update(delta)
+#         # self.robin_sprite.set_position(self.world.robin.x, self.world.robin.y)
--- models.py
+++ models.py
@@ -15,3 +15,32 @@ class World():
 
     def update(self, delta):
         pass
+
+# class Robin(Model):
+#     DIR_HORIZONTAL = 0
+#     DIR_VERTICAL = 1
+ 
+#     def __init__(self, world, x, y):
+#         super().__init__(world, x, y, 0)
+ 
+#         self.direction = Robin.DIR_VERTICAL
+ 
+ 
+#     def switch_direction(self):
+#         if self.direction == Robin.DIR_HORIZONTAL:
+#             self.direction = Robin.DIR_VERTICAL
+#             self.angle = 0
+#         else:
+#             self.direction = Robin.DIR_HORIZONTAL
+#             self.angle = -90
+ 
+ 
+#     def update(self, delta):
+#         if self.direction == Robin.DIR_VERTICAL:
+#             if self.y > self.world.height:
+#                 self.y = 0
+#             self.y += 5
+#         else:
+#             if self.x > self.world.width:
+#                 self.x = 0
+#             self.x += 5
